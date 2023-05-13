add593872511e11e124b1f4badb545e0e36af121
rolandi39 <rolandinachke@gmail.com>
2023-05-14 01:54:27 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '822622bc-c593-4e4d-860a-7a6a9574cd15';
+export const MODAL_CLOSE_SEC = 2.5;
